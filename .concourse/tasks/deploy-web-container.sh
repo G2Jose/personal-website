381bb3c601e74c9f517f686744186bcd3ce8706b
George Jose <gejose@deloitte.ca>
2017-02-23 01:43:47 -0500

    concourse updates

--- .concourse/tasks/deploy-web-container.sh
+++ .concourse/tasks/deploy-web-container.sh
@@ -89,7 +89,7 @@ eval $(ssh-agent -s)
 echo "$DEPLOY_SSH_KEY" > ssh_key
 cat ssh_key && chmod 400 ssh_key && ssh-add ssh_key
 mkdir -p ~/.ssh
-# '[[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config'
+'[[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config'
 ssh $SSH_HOST "docker login --username=$DOCKER_HUB_USERNAME --password=$DOCKER_HUB_PASSWORD && docker pull $DOCKER_HUB_DEPLOY_TAG && docker run -p 80:8080 -dt $DOCKER_HUB_DEPLOY_TAG"
 # docker login --username=$DOCKER_HUB_USERNAME --password=$DOCKER_HUB_PASSWORD
 echo "done deploying docker container"
